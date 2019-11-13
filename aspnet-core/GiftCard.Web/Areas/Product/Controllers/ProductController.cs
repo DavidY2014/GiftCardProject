0ec54e94b5cf029ecc8b3f5568c7fa351e46388b
袁大伟 <yuandawei@colipu.com>
2019-11-13 14:11:38 +0800

    fix

---
+++ aspnet-core/GiftCard.Web/Areas/Product/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiftCard.Web.Areas.Product.Controllers
+{
+    [Area("Product")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
